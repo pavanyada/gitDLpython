f7c2293b98ebb719711d1739e019d7115fbe10cf
pavanyada <pavan24.dl@gmail.com>
2018-07-18 12:35:34 +0530

    fourth

--- practice.py
+++ practice.py
@@ -8,7 +8,16 @@
 # 	print(a+b)
 # addSum(10,20)
 
-l1=[1,2,3,4]
-l2=[10,20,30,40]
-associativeSum=list(filter(lambda a,b:a+b,l1,l2))
-print(associativeSum)
+# l1=[1,2,3,4]
+# l2=[10,20,30,40]
+# associativeSum=list(filter(lambda a,b:a+b,l1,l2))
+# print(associativeSum)
+
+
+
+# for i in range(1,11):
+# 	print("5 * %d =%d" %(i,5*i))
+
+a=10
+for i in range(1,a,2):
+	print(" "*((a-i)//2)+"*"*i)
